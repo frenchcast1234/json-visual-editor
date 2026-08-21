cecb72c6229f79bf0e741fcdf3e4e510cc7e6e33
frenchcast1234 <loan.walgenwitz@gmail.com>
2026-08-21 02:41:34 +0200

    Add recent file with SharedPreferences

--- lib/ui/menu.dart
+++ lib/ui/menu.dart
@@ -3,9 +3,9 @@ import 'dart:convert';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:json_visual_editor/pages/editor.dart';
 import 'package:json_visual_editor/ui/tab_bar.dart';
 import 'package:menu_bar/menu_bar.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Menu extends StatefulWidget {
   const Menu({super.key});
@@ -14,8 +14,20 @@ class Menu extends StatefulWidget {
   State<Menu> createState() => _MenuState();
 }
 
-class _MenuState extends State<Menu> {
+class _MenuState extends State<Menu>  {
   late final GlobalKey<TabBarEditorState> _tabBarKey = GlobalKey<TabBarEditorState>();
+  late SharedPreferences prefs;
+  final _fileName = RegExp(r'[^/\\]+$');
+  
+  @override void initState() {
+    super.initState();
+    _loadPrefs();
+  }
+
+  Future<void> _loadPrefs() async {
+  final p = await SharedPreferences.getInstance();
+  setState(() => prefs = p);
+}
 
   @override
   Widget build(BuildContext context) {
@@ -40,8 +52,16 @@ class _MenuState extends State<Menu> {
                     File f = File(file.path!);
                     String tmp = await f.readAsString();
                     _tabBarKey.currentState?.addTab(tmp, file.name, file.path!);
+                    setState(() {
+                      prefs.setStringList("recent_files", (prefs.getStringList("recent_files") ?? [])..add(file.path!));
+                    });
                   }
                 },
+              ),
+              MenuButton(
+                text: const Text("Open recent file"),
+                icon: const Icon(Icons.history),
+                submenu: SubMenu(menuItems: getRecentFiles().isEmpty ? [ MenuButton(text: Text("No recent file")) ] : getRecentFiles())
               ),
               MenuButton(
                 text: const Text("Save file"),
@@ -75,4 +95,25 @@ class _MenuState extends State<Menu> {
       child: TabBarEditor(key: _tabBarKey),
     );
   }
+
+  List<MenuButton> getRecentFiles() {
+    List<MenuButton> s = [];
+
+    for (String x in prefs.getStringList('recent_files') ?? []) {
+      s.add(
+        MenuButton(
+          text: Text(x),
+          onTap: () async {
+            File f = File(x);
+            String tmp = await f.readAsString();
+            _tabBarKey.currentState?.addTab(tmp, nameOf(x), x);
+          },
+        )
+      );
+    }
+
+    return s;
+  }
+
+  String nameOf(String path) => _fileName.firstMatch(path)?.group(0) ?? path;
 }
